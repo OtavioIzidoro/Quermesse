b79b48abf161f8c45ecc055a3bff58771ef77bb8
otavio.izidoro <otavio.izidoro@pumpkintech.com.br>
2022-10-30 13:55:08 -0300

    Feature[CreateUser]: Criação da pagina de cadastro do usuario

---
+++ src/Styles/createUser.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const CreateUserFormFundo = styled.div`
+width: 100vw;
+height: 100vh;
+background: #6C7A89;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center; 
+`;
+
+
+export const CreateUserForm= styled.div`
+width: 50%;
+height: 70%;
+background: #fff;
+
+`
+
+export const FormCreateUser = styled.div`
+width: 35vw;
+height: 50vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+.formSenha{
+  margin-top:15px;
+}
+.button{
+  margin-top:10px
+}
+.form{
+  width:20rem
+}
+
+`
