456af88cd7987cc0a9d0722c83f5d404c00ab2a0
otavio.izidoro <otavio.izidoro@pumpkintech.com.br>
2022-11-03 23:48:58 -0300

    Criação dos Input de Login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,38 +1,8 @@
-import { Form, LoginForm, LoginFundo } from "../Styles/Login";
+import InputLoginValidation from "../Components/Input/index";
+
 
 export default function Login() {
   return (
-    <LoginFundo>
-      <LoginForm>
-        <a href="/">Voltar</a>
-        <Form>
-          <div className="form-group formEmail">
-            <label>Email:</label>
-            <input
-              type="email"
-              className="form-control form "
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            
-          </div>
-          <div className="form-group formSenha">
-            <label>Senha:</label>
-            <input
-              type="password"
-              className="form-control form"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary button">
-            Acessar
-          </button>
-          <a href="/Createuser">CRIAR USUARIO</a>
-        </Form>
-        
-      </LoginForm>
-    </LoginFundo>
+    <InputLoginValidation/>
   );
 }
